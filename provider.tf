44d50d81ab7580a67f3d1b2a6d056d25d7752807
naveen <navinb3236@gmail.com>
2025-10-16 07:38:40 +0000

    terraform

--- provider.tf
+++ provider.tf
@@ -19,11 +19,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-
-  features {}
-}
-provider "azurerm" {
-  # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
   subscription_id = var.subscription_id
   client_id = var.client_id
